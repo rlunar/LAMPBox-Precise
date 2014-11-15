9a1a82b6a2f4bdb94cbe63d88d6ac87c43cedf0b
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2014-11-15 17:08:18 -0500

    Update phpmyadmin.sh

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -1,6 +1,5 @@
-sudo add-apt-repository ppa:nijel/phpmyadmin
+sudo add-apt-repository -y ppa:nijel/phpmyadmin
 sudo apt-get update
-sudo apt-get install php5-mysqlnd
 
 sudo apt-get remove  phpmyadmin --purge
 sudo apt-get install phpmyadmin
