e142182203bf2d546a9c350c2a1bae3b8d9c65f3
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2014-11-15 16:00:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,14 +35,14 @@ sudo sed -i "s/#ServerRoot.*/ServerName VagrantBox/" /etc/apache2/apache2.conf
 sudo a2enmod rewrite
 
 # Enable PHP Error Reporting
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/cli/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/cli/php.ini
-sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/cli/php.ini
-sed -i "s/;date.timezone.*/date.timezone = America\/New_York/" /etc/php5/cli/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/cli/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/cli/php.ini
+sudo sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/cli/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = America\/New_York/" /etc/php5/cli/php.ini
 
 sudo service apache2 restart
 
