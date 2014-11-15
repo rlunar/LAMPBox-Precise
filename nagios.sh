1ea963d5078542f106e1eb0984e6bc86080e1f67
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2014-11-15 16:34:39 -0500

    Update nagios.sh

--- nagios.sh
+++ nagios.sh
@@ -3,4 +3,4 @@
 # Install Nagios
 sudo apt-get install -y nagios3 nagios-nrpe-plugin
 # To add a user:
-sudo htpasswd /etc/nagios3/htpasswd.users rluna
+sudo htpasswd /etc/nagios3/htpasswd.users vagrant
